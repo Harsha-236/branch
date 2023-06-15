d463572ff13effdaf62edf4533550b370b8d1e8d
harsha <goldenharsha143@gmail.com>
2023-06-16 03:06:58 +0530

    modified hello world to palindrome string

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print('hello harsha')
+a='radar'
+b='harsha'
+
+print(type(a),type(b))
+
+# Check - Palindrome
+
+print(a==a[::-1]) # 'radar' == 'radar -True
+print(b==b[::-1]) # 'harsha'== 'ahsrah' -False
